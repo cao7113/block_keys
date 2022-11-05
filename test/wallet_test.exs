8c3883aed83ba71c04a44d56107df2a1fc4d29d0
Ruijian Cao <caoruijian@big.one>
2022-11-05 21:54:22 +0800

    fix

--- test/wallet_test.exs
+++ test/wallet_test.exs
@@ -9,8 +9,25 @@ defmodule WalletTest do
 
       root_key = BlockKeys.from_mnemonic(mnemo)
 
-      BlockKeys.CKD.derive(root_key, "M/44'/60'/0'/0/0")
-      |> IO.inspect(label: "derive key")
+      path = "M/44'/60'/0'/0/0"
+
+      xpub =
+        BlockKeys.CKD.derive(root_key, path)
+        |> IO.inspect(label: "derive xpub key")
+
+      xpub
+      |> BlockKeys.Ethereum.Address.from_xpub()
+      |> IO.inspect(label: "eth address")
+
+      xpub
+      |> BlockKeys.Ethereum.Address.from_xpub()
+      |> IO.inspect(label: "eth address again")
+
+      BlockKeys.CKD.derive(root_key, "M/44'/60'/0'/0/1")
+      |> BlockKeys.Ethereum.Address.from_xpub()
+      |> IO.inspect(label: "eth address1")
+
+      # "0x73bb50c828fd325c011d740fde78d02528826156"
     end
   end
 end
